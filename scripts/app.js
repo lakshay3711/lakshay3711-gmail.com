a7cd938534935b9694967dbbdfaadaaff0c72009
Lakshay <lakshay3711@gmaol.com>
2020-04-02 10:38:35 +0530

    allmost last commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,41 @@
 const cityform = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 const updateui = (data) => {
     const citydets = data.citydets;
     const weather = data.weather;
+
+
     details.innerHTML = ` <h5 class="my-3">${citydets.EnglishName}</h5>
               <div class="my-3">${weather.WeatherText}</div>
                 <div class="display-4 my-4">
-                    <span>${weather.Temperature.Metric.Value}</span>
-                    <span>&deg;</span> </div>`
-    if(card.classList.contains('d-none')){
-        card.classList.remove('d-none');
-        
+                    <span>${weather.Temperature.Metric.Value}</span><span>&deg;</span> </div>`
+
+
+    // update day and night $ icons
+    const iconsrc =`img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src',iconsrc);
+
+    let timesrc = null;
+    if (weather.IsDayTime) {
+        timesrc = 'img/day.svg';
+    } else {
+        timesrc = 'img/night.svg';
     }
-    
-    
+
+    time.setAttribute('src', timesrc);
+
+
+
+
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+
+    }
+
+
 };
 const updatecity = async (city) => {
     const citydets = await getcity(city);
@@ -25,6 +46,9 @@ const updatecity = async (city) => {
     };
 
 }
+
+
+
 cityform.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -33,9 +57,9 @@ cityform.addEventListener('submit', e => {
 
     updatecity(city)
 
-        .then(data => {console.log(data);
-        updateui(data);
-                      })
+        .then(data => {
+            updateui(data);
+        })
         .catch(err => console.log(err));
 
 });
